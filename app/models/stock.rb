9483386280870c84438d327a214f30ea52a58d31
anthonygharvey <anthony@anthonygharvey.com>
2019-05-26 03:34:19 -0400

    Added RestClient and updated Stock model
    Added RestClient gem for API calls
    Added Alpha Vantage api key
    Added Stock#get_prices and #unique_symbol to only
    fetch stock prices from Alpha Vantage for unquie stocks
    and save with only date and price

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,22 @@
 class Stock < ApplicationRecord
+	require 'json'
+	validates :symbol, uniqueness: true
+	before_save :get_prices, if: :unique_symbol
+
+	def unique_symbol
+		!Stock.pluck(:symbol).include?(self.symbol)
+	end
+
+	def get_prices
+		response = RestClient.get 'https://www.alphavantage.co/query', {params: {
+			function: 'TIME_SERIES_DAILY_ADJUSTED',
+			symbol: "#{symbol}",
+			outputsize: "full",
+			apikey: ENV['ALPHA_VANTAGE_API_KEY']
+		}}
+		stock = JSON.parse(response)
+		self.prices = stock["Time Series (Daily)"].map do |k,v|
+			{date: k.to_date, price: v.values[4].to_f}
+		end
+	end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+	config.alpha_vest_api_key = ENV.fetch('ALPHA_VANTAGE_API_KEY')
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
