7e7ac56d1ae39ad1ba4a94817972b2ffb882a40b
anthonygharvey <anthony@anthonygharvey.com>
2019-05-26 23:52:33 -0400

    Added random_stock route and stock_controller

---
+++ app/controllers/api/stocks_controller.rb
@@ -0,0 +1,10 @@
+class Api::StocksController < ApplicationController
+	def random
+			random_stock = Stock.random_stock
+			render json: {
+				"symbol": random_stock.symbol,
+				"prices": random_stock.random_dates
+			}
+	end
+
+end
