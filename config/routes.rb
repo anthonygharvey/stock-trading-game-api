c2ee2a0bf6bbc268dbe67c38396fbb250a741d32
anthonygharvey <anthony@anthonygharvey.com>
2019-07-03 00:09:41 -0400

    Added leaderboard api endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
 	namespace :api do
 		get 'random_stock', to: 'stocks#random'
 		post 'save_game', to: 'players#create'
+		get 'leaderboard', to: 'portfolios#leaderboard'
 	end
 end
