2626899338af612438b13b025b6d3602a58722b3
anthonygharvey <anthony@anthonygharvey.com>
2019-07-06 15:56:53 -0400

    Updated cors origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://anonymous-stock-client.herokuapp.com/'
+    origins 'anonymous-stock-client.herokuapp.com'
 
     resource '*',
       headers: :any,
